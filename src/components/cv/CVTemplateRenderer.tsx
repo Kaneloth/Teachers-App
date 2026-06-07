0f69e648507097805c3f1fc2229f299d6f4747c7
Kaneloth <kanelothelejane@gmail.com>
2026-06-07 11:09:04 +0200

    Update CVTemplateRenderer.tsx

--- src/components/cv/CVTemplateRenderer.tsx
+++ src/components/cv/CVTemplateRenderer.tsx
@@ -171,7 +171,7 @@ function Section({ title, color, borderColor, icon, children }: { title: string;
             display: 'inline-block',
             verticalAlign: 'middle',
             position: 'relative',
-            top: '-3px', // increased from -1px to lift icon further
+            top: '-3px', // shift icon up to align with text baseline
           }}>
             {icon}
           </span>
